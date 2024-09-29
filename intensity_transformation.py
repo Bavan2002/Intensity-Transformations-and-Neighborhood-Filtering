f490e97ca45aa45af26a671a5d96b8e5e84e275c
Sundarbavan <sundarbavan2002@gmail.com>
2024-09-29 10:09:55 +0530

    intensity_transformation.py created

---
+++ intensity_transformation.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+c = np.array([(100, 50), (150, 200)])
+t1 = np.linspace(0, c[0,1], c[0,0] + 1 - 0).astype('uint8')
+print(len(t1))
+t2 = np.linspace(c[0,1] + 1, c[1,1], c[1,0] - c[0,0]).astype('uint8')
+print(len(t2))
+t3 = np.linspace(c[1,1] + 1, 255, 255 - c[1,0]).astype('uint8')
+print(len(t3))
+transform = np.concatenate((t1, t2), axis=0).astype('uint8')
+transform = np.concatenate((transform, t3), axis=0).astype('uint8')
+print(len(transform))
+
+plt.plot(transform)
+plt.xlabel('Input, $f(x)$')
+plt.ylabel('Output, $T[f(x)]$')
+plt.xlim(0,255)
+plt.ylim(0,255)
+plt.savefig('transform.png')
+plt.show()
+
+img_orig = cv.imread('a1images/daisy.jpg', cv.IMREAD_GRAYSCALE)
+cv.namedWindow("Image", cv.WINDOW_AUTOSIZE)
+cv.imshow("Image", img_orig)
+cv.waitKey(0)
+# image_transformed = cv.LUT(img_orig, transform)
+# cv.imshow("Image", image_transformed)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
